227c720adf813738389f80b7d6fb8ec3773529de
Vitali <basaraba.vitalii@cnhu.edu.ua>
2024-10-23 21:48:53 +0300

    lab3

--- mysite/settings.py
+++ mysite/settings.py
@@ -117,7 +117,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # або інше, залежно від ваших налаштувань
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # цей каталог може містити ваші статичні файли
+]
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
